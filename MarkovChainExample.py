5d27edb52206b2d033d18fdf0386fc3dcbfcb2f1
dt2510 <33524923+dt2510@users.noreply.github.com>
2017-11-09 13:26:05 -0500

    Update MarkovChainExample.py

--- MarkovChainExample.py
+++ MarkovChainExample.py
@@ -4,40 +4,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def MarkovMatrix(alpha, beta):
-    #alpha = P(unemployed worker finds a job)
-    #beta = P(employed worker loses a job)
+    
+    '''
+    Description: Constructs a stochastic matrix for employement transitions
+    
+    Inputs:
+        alpha: Probability unemployed worker finds a job
+        beta: Probability employed worker loses a job
+    '''
+    
     return np.array([[1-alpha, alpha],[beta, 1-beta]])
 
-#Construct a particular matrix
-
-alpha, beta = 0.3, 0.05
-P = MarkovMatrix(alpha, beta)
-
-#Simulate the Markov chain for T periods
-
-states = np.array((0,1))
-init_state = states[1] #Begin unemployed
-num_periods = 250
-chain = np.zeros(num_periods, dtype = int)
-chain[0] = init_state
-for i in range(1,num_periods):
-    chain[i] = np.random.choice(states,1, p = P[chain[i-1]])
+def Simulation(alpha, beta, x_0, T):
     
-#Plot the resulting chain and the fraction of time employed   
+    '''
+    Description: Plots the Markov Chain for T periods
+    
+    Inputs:
+        alpha, beta: MC parameters (between 0 and 1)
+        x_0: initial state (0 or 1)
+        T: number of periods (positive integer)
+    '''
+    
+    #Simulate the chain
+    P = MarkovMatrix(alpha, beta)
+    chain = np.zeros(T, dtype = int)
+    chain[0] = x_0
+    for i in range(1,T):
+        chain[i] = np.random.choice(states,1, p = P[chain[i-1]])
+        
+    #Plot the chain and the fraction of time employed
+    fig, ax = plt.subplots(figsize = (8,6))
+    periods = np.arange(T)
+    emp_frac = np.cumsum(chain)/(1+periods)
+    ax.plot(periods, chain, label = 'Employment State')
+    ax.plot(periods, emp_frac, label = 'Cumulative Employment')
+    ax.set_title('Markov Chain Simulation')
+    ax.set_xlabel('Time Period')
+    plt.legend(loc = 3)
+    plt.show()
+    
+def TransitionMatrix(alpha, beta, K):
+    
+    '''
+    Description: Returns the K-period transition matrix
+    
+    Inputs:
+        alpha, beta: MC parameters (between 0 and 1)
+        K: number of periods (positive integer)
+    '''
+    
+    P = MarkovMatrix(alpha, beta)
+    return np.linalg.matrix_power(P,i)
+    
+#Run a simulation
+Simulation(.5,.25,0,250)
 
-fig, ax = plt.subplots(figsize = (8,6))
-periods = np.arange(num_periods)
-ax.plot(periods, chain, label = 'Employment State')
-emp_frac = np.cumsum(chain)/(1+periods)
-ax.plot(periods, emp_frac, label = 'Cumulative Employment')
-ax.set_title('Markov Chain Simulation')
-ax.set_xlabel('Time Period')
-plt.legend(loc = 3)
-plt.show()
-
-
-#Plot the transition matrix for many periods ahead
-
+#Plot the transition matrix for several periods ahead:
 np.set_printoptions(precision = 2)
-for i in range(1,15):
-    print('The %s period transition matrix is:\n %s'%(i, np.linalg.matrix_power(P,i)))
+for i in range(1,10):
+    print('The %s period transition matrix is:\n %s'%(i, TransitionMatrix(0.5,0.25,i)))
