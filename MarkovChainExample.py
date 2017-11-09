ee23f383a07bd9a3a3dc19882e37d2497818074e
dt2510 <33524923+dt2510@users.noreply.github.com>
2017-11-09 12:43:52 -0500

    Update and rename MarkovChainExample.ipynb to MarkovChainExample.py

--- MarkovChainExample.py
+++ MarkovChainExample.py
@@ -9,19 +9,22 @@ def MarkovMatrix(alpha, beta):
     return np.array([[1-alpha, alpha],[beta, 1-beta]])
 
 #Construct a particular matrix
-alpha, beta = 0.5, 0.25
+
+alpha, beta = 0.3, 0.05
 P = MarkovMatrix(alpha, beta)
 
 #Simulate the Markov chain for T periods
+
 states = np.array((0,1))
 init_state = states[1] #Begin unemployed
-num_periods = 1000
+num_periods = 250
 chain = np.zeros(num_periods, dtype = int)
 chain[0] = init_state
 for i in range(1,num_periods):
     chain[i] = np.random.choice(states,1, p = P[chain[i-1]])
     
-#Plot the resulting chain and the fraction of time employed    
+#Plot the resulting chain and the fraction of time employed   
+
 fig, ax = plt.subplots(figsize = (8,6))
 periods = np.arange(num_periods)
 ax.plot(periods, chain, label = 'Employment State')
@@ -32,7 +35,9 @@ ax.set_xlabel('Time Period')
 plt.legend(loc = 3)
 plt.show()
 
-#Plot the transition matrix for many periods ahead:
+
+#Plot the transition matrix for many periods ahead
+
 np.set_printoptions(precision = 2)
 for i in range(1,15):
     print('The %s period transition matrix is:\n %s'%(i, np.linalg.matrix_power(P,i)))
